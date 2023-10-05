a483808f06fb786acc65cbec97757978f0cc3791
nachthelad <nachthelad@gmail.com>
2023-10-05 20:55:06 -0300

    PreEntrega1-Ventura

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './NavBar.css'
+
+const NavBar = () => {
+  return (
+    <div>
+      <navbar>
+        <ul>
+          <li><a href="/#">Home</a></li>
+        </ul>
+      </navbar>
+    </div>
+  );
+};
+
+export default NavBar;
